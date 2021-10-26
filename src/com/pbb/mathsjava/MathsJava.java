fc7bde7b8e15ebbd8082dbda8aa66d4bdb720225
priyankabellary <priyankabbellary@gmail.com>
2021-10-26 23:08:27 +0530

    init

---
+++ src/com/pbb/mathsjava/MathsJava.java
@@ -0,0 +1,11 @@
+package com.pbb.mathsjava;
+
+public class MathsJava {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 5;
+        int c = a - b;
+        System.out.println(c);
+    }
+}
+                                                                                                                                                                             nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
